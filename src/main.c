631e6444b0161ef151ac08e79d9877b059125600
Tamsen1995 <Tamanhbui95@gmail.com>
2018-04-11 12:46:11 +0200

    [ADD] Implementing a function which will display the state of our allocated memory zones.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,31 @@
 #include "../includes/ft_malloc.h"
 
+/*
+** shows the status of the allocation zones
+
+	TINY : 0xA0000
+	0xA0020 - 0xA004A : 42 bytes
+	0xA006A - 0xA00BE : 84 bytes
+	SMALL : 0xAD000
+	0xAD020 - 0xADEAD : 3725 bytes
+	LARGE : 0xB0000
+	0xB0020 - 0xBBEEF : 48847 bytes
+	Total : 52698 bytes
+*/
+
+void show_alloc_mem()
+{
+	t_block *tmp_block;
+
+	ft_putstr("TINY : ");
+	printf("%p", glob_memory.sml); // to be refactored
+	tmp_block = glob_memory.sml->mem;
+	while (tmp_block)
+	{
+		tmp_block = tmp_block->next;
+	}
+}
+
 int main(void)
 {
 	char *str;
@@ -22,6 +48,8 @@ int main(void)
 
 	str = (char *)ft_realloc(str, nbr2);
 
-	ft_putstr(str);
+	show_alloc_mem();
+
+	//ft_putstr(str);
 	return (0);
 }
