a5dec2a82b307563a2ae897b7446da3470c402ea
Tamsen1995 <Tamanhbui95@gmail.com>
2018-04-11 20:04:41 +0200

    [UPD] The free function has been properly implemented. I still need to improve upon my check_pointer function

--- src/main.c
+++ src/main.c
@@ -1,67 +1,18 @@
 #include "../includes/ft_malloc.h"
 
-/*
-** shows the status of the allocation zones
-
-	TINY : 0xA0000
-	0xA0020 - 0xA004A : 42 bytes
-	0xA006A - 0xA00BE : 84 bytes
-	SMALL : 0xAD000
-	0xAD020 - 0xADEAD : 3725 bytes
-	LARGE : 0xB0000
-	0xB0020 - 0xBBEEF : 48847 bytes
-	Total : 52698 bytes
-*/
-
-void show_alloc_mem()
+void ft_free(void *ptr)
 {
 	t_block *tmp_block;
 
-	ft_putstr("TINY : ");
-	printf("%p\n", glob_memory.sml); // to be refactored
-	tmp_block = glob_memory.sml->mem;
-	while (tmp_block)
+	tmp_block = NULL;
+	if (!check_pointer(ptr))
 	{
-		printf("%p - ", tmp_block->ptr);
-		printf("%p : ", tmp_block->next);
-		printf("%lu bytes\n", tmp_block->size);
-		if (tmp_block->free == TRUE)
-			ft_putendl("block status: FREE");
-		else
-			ft_putendl("block status: NOT FREE");
-		tmp_block = tmp_block->next;
+		ft_putendl("Please provide a valid pointer.");
+		return;
 	}
-
-	ft_putstr("\nSMALL : ");
-	printf("%p\n", glob_memory.sml); // to be refactored
-
-	tmp_block = glob_memory.med->mem;
-	while (tmp_block)
-	{
-		printf("%p - ", tmp_block->ptr);
-		printf("%p : ", tmp_block->next);
-		printf("%lu bytes\n", tmp_block->size);
-		if (tmp_block->free == TRUE)
-			ft_putendl("block status: FREE");
-		else
-			ft_putendl("block status: NOT FREE");
-		tmp_block = tmp_block->next;
-	}
-
-	ft_putstr("\nLARGE : ");
-	printf("%p\n", glob_memory.sml); // to be refactored
-	tmp_block = glob_memory.large;
-	while (tmp_block)
-	{
-		printf("%p - ", tmp_block->ptr);
-		printf("%p : ", tmp_block->next);
-		printf("%lu bytes\n", tmp_block->size);
-		if (tmp_block->free == TRUE)
-			ft_putendl("block status: FREE");
-		else
-			ft_putendl("block status: NOT FREE");
-		tmp_block = tmp_block->next;
-	}
+	tmp_block = (void *)ptr - sizeof(t_block);
+	tmp_block->free = TRUE;
+	defragment(tmp_block);
 }
 
 int main(void)
@@ -85,6 +36,9 @@ int main(void)
 	}
 	str[i] = '\0';
 	str = (char *)ft_realloc(str, nbr2 + 1);
+
+	ft_free(str);
+
 	show_alloc_mem(); // TESTING
 	//ft_putstr(str);   // TESTING
 	return (0);
