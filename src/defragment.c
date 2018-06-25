4245e829ac3b3ac5434820309274741d04a14a11
Tamanh BUI <tbui@e2r6p1.42.fr>
2018-06-25 18:22:17 +0200

    [NORM]

--- src/defragment.c
+++ src/defragment.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   defragment.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbui <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/25 17:57:57 by tbui              #+#    #+#             */
+/*   Updated: 2018/06/25 17:57:59 by tbui             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/ft_malloc.h"
 
 /*
@@ -5,23 +17,22 @@
 ** starts from the beginning of the list and the traverses the entire memory
 */
 
-void defragment(t_block *block_list)
+void		defragment(t_block *block_list)
 {
 	t_block *tmp_block;
 
 	tmp_block = block_list;
 	while (tmp_block && tmp_block->next)
 	{
-
-		if (tmp_block && tmp_block->free == TRUE && tmp_block->next && tmp_block->next->free == TRUE)
+		if (tmp_block && tmp_block->free == TRUE && \
+		tmp_block->next && tmp_block->next->free == TRUE)
 		{
-
-			tmp_block->size = tmp_block->size + tmp_block->next->size + sizeof(t_block);
+			tmp_block->size = tmp_block->size + \
+			tmp_block->next->size + sizeof(t_block);
 			tmp_block->next = tmp_block->next->next;
 			if (!tmp_block->next)
-				break;
+				break ;
 		}
-
 		tmp_block = tmp_block->next;
 	}
 }
--- src/free.c
+++ src/free.c
@@ -1,14 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbui <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/25 17:59:05 by tbui              #+#    #+#             */
+/*   Updated: 2018/06/25 17:59:07 by tbui             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/ft_malloc.h"
 
-void free(void *ptr)
+void	free(void *ptr)
 {
 	t_block *tmp_block;
 
 	tmp_block = NULL;
 	if (!check_pointer(ptr))
-		return;
+		return ;
 	tmp_block = ptr - sizeof(t_block);
 	tmp_block->free = TRUE;
 	defragment(tmp_block);
-
 }
