5f51128434c82f94442baef469b2da78d4117a66
Tamsen1995 <Tamanhbui95@gmail.com>
2018-06-26 15:15:39 +0200

    [UPD] Everything in check. Only a couple logic checks to undergo before turning in now.

--- src/show_alloc_mem.c
+++ src/show_alloc_mem.c
@@ -49,7 +49,8 @@ void show_alloc_zone(enum mem_zone zone)
 			ft_putstr(" - ");
 			ft_put_addr((void *)tmp_block->next);
 			ft_putstr(" : ");
-			printf("%lu bytes\n", tmp_block->size); // Refactor
+			ft_putnbr((int)tmp_block->size);
+			ft_putendl(" bytes\n");
 			if (tmp_block->free == TRUE)
 				ft_putendl("block status: FREE\n");
 			else
@@ -72,9 +73,12 @@ void show_alloc_mem()
 	tmp_block = glob_memory.large;
 	while (tmp_block)
 	{
-		printf("%p - ", tmp_block->ptr);
-		printf("%p : ", tmp_block->next);
-		printf("%lu bytes\n", tmp_block->size);
+		ft_put_addr(tmp_block->ptr);
+		ft_putstr(" - ");
+		ft_put_addr(tmp_block->next);
+		ft_putstr(" : ");
+		ft_putnbr((int)tmp_block->size);
+		ft_putendl(" bytes\n");
 		if (tmp_block->free == TRUE)
 			ft_putendl("block status: FREE\n");
 		else
